ec595bfcbdc290e4ca2d480b793acd120d29f55f
debuggingfuture <lauchunyin@debuggingfuture.com>
2024-12-12 19:08:05 +0800

    fix: cloudflare deployment

--- apps/cms/astro.config.mjs
+++ apps/cms/astro.config.mjs
@@ -8,6 +8,16 @@ import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+	output: 'server',
+	vite: {
+		ssr: {
+			target: 'node',
+			// per target Node.js built-ins will also be externalized by default.
+			// seems not working with cloudflare adapter, explicit for now
+			external: ['crypto', 'node:util', 'node:process', 'node:buffer', 'node:fs', 'node:path', 'node:crypto',
+				'node:assert', 'node:events', 'node:os', 'node:stream', 'node:http', 'node:https', 'node:zlib', 'node:net'],
+		}
+	},
 	integrations: [
 		react({
 			experimentalReactChildren: true,
